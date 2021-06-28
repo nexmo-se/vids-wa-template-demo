d023f4881d511b8488e40367b749b8e8ecf53e9b
Frans Huang <franssiswanto@gmail.com>
2021-06-28 15:10:50 +0800

    Able to send message based on template

--- frontend/src/components/SendMessageButton/index.tsx
+++ frontend/src/components/SendMessageButton/index.tsx
@@ -5,77 +5,50 @@ import { useTemplate } from "components/TemplateProvider";
 
 function SendMessageButton() {
   const [sending, setSending] = useState<boolean>(false);
-  const { selectedTemplate } = useTemplate();
+  const { selectedTemplate, targetPhoneNumber } = useTemplate();
 
   async function handleSendMessageClick() {
     try {
       setSending(true);
 
-      if (selectedTemplate) {
-        const url = `${Config.apiUrl}/whatsapp-templates`;
-        const body = JSON.stringify({
-          from: {
-            type: "whatsapp",
-            number: "447418342132"
-          },
-          to: {
-            type: "whatsapp",
-            number: "6585773051"
-          },
-          content: {
-            type: "custom",
-            custom: {
-              type: "template",
-              template: {
-                namespace: "whatsapp:hsm:technology:nexmo",
-                name: "airline_ticket_update",
-                language: {
-                  policy: "deterministic",
-                  code: "en"
-                },
-                components: [
-                  {
-                    type: "header",
-                    parameters: [{
-                      type: "document",
-                      document: {
-                        link: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
-                      }
-                    }]
-                  },
-                  {
-                    type: "body",
-                    parameters: [
-                      {
-                        type: "text",
-                        text: "Frans Siswanto"
-                      },
-                      {
-                        type: "text",
-                        text: "GA 123"
-                      },
-                      {
-                        type: "text",
-                        text: "Surabaya"
-                      },
-                      {
-                        type: "text",
-                        text: "Singapore"
-                      }
-                    ]
-                  }
-                ]
-              }
+      if (!selectedTemplate) return;
+      if (!targetPhoneNumber) return;
+    
+      const url = `${Config.apiUrl}/whatsapp-templates`;
+      const body = JSON.stringify({
+        from: {
+          type: "whatsapp",
+          number: "447418342132"
+        },
+        to: {
+          type: "whatsapp",
+          number: targetPhoneNumber
+        },
+        content: {
+          type: "custom",
+          custom: {
+            type: "template",
+            template: {
+              namespace: selectedTemplate.namespace,
+              name: selectedTemplate.id,
+              language: {
+                policy: "deterministic",
+                code: "en"
+              },
+              components: [
+                selectedTemplate.headerPayload(),
+                selectedTemplate.bodyPayload()
+              ]
             }
           }
-        })
+        }
+      })
 
-        await fetch(url, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body
-        });
-      }
+      await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body
+      });
     } catch (err) {
       console.error(err);
     } finally {
--- frontend/src/components/TemplateProvider/models/template.ts
+++ frontend/src/components/TemplateProvider/models/template.ts
@@ -1,5 +1,15 @@
 import { HeaderType } from "../types";
 
+type Paramater = {
+  type: string;
+  text: string;
+}
+
+interface BodyPayload {
+  type: "body",
+  parameters: Paramater[]
+}
+
 interface ITemplate {
   id: string;
   namespace: string;
@@ -21,6 +31,56 @@ class Template implements ITemplate {
     this.header = args.header;
     this.body = args.body;
     this.footer = args.footer;
+  }
+
+  headerPayload () {
+    if (!this.header) return null;
+
+    const mediaType = ["document", "image", "video"];
+    if (mediaType.includes(this.header.value)) {
+      return {
+        type: "header",
+        parameters: [{
+          type: this.header.value,
+          [this.header.value]: {
+            link: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+          }
+        }]
+      }
+    } else if(this.header.value === "location") {
+      return {
+        type: "header",
+        parameters: [{
+          type: this.header.value,
+          [this.header.value]: {
+            longitude: -122.425332,
+            latitude: 37.758056,
+            name: "Facebook HQ",
+            address: "1 Hacker Way, Menlo Park, CA 94025"
+          }
+        }]
+      }
+    } else return null;
+  }
+
+  bodyPayload () {
+    const parameters = this.body.split(/{{\d+}}/g);
+    const basePayload: BodyPayload = {
+      type: "body",
+      parameters: []
+    }
+
+    parameters.forEach(
+      (_, index) => {
+        if (index === 0) return;
+        basePayload.parameters.push({
+          type: "text",
+          text: `${index}`
+        })
+      }
+    )
+
+    return basePayload;
   }
 }
 
