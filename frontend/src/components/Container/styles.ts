9a6b7d5976775bd8551b1d29536fe9db8e350ebd
Frans Siswanto <franssiswanto@gmail.com>
2021-06-30 10:20:33 +0800

    Optimising for VIDS

--- frontend/src/components/Container/styles.ts
+++ frontend/src/components/Container/styles.ts
@@ -1,7 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   container: {
-    padding: theme.spacing(4),
     margin: "0 auto",
     height: "100%"
   }
